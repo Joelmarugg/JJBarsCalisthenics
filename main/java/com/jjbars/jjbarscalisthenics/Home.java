57d0ed86ee7cc2a952172b8ba5c08a4b60b101db
Joel Marugg <joel.marugg@students.fhnw.ch>
2018-05-07 11:53:36 +0200

    show workouts possible

--- main/java/com/jjbars/jjbarscalisthenics/Home.java
+++ main/java/com/jjbars/jjbarscalisthenics/Home.java
@@ -3,6 +3,8 @@ package com.jjbars.jjbarscalisthenics;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 
 public class Home extends AppCompatActivity {
@@ -14,6 +16,9 @@ public class Home extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
         setContentView(R.layout.activity_home);
 
         openCreateCustomWorkout =(Button)findViewById(R.id.create_custom_workout_button);
